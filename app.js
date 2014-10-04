d994e7f65a4604ebe6d8d585654e41afcbb7d02a
iagowp <iago.wp@hotmail.com>
2014-10-04 10:27:17 -0700

    changed lang to american english, added comments

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
 var song;
-var recognition = new webkitSpeechRecognition();
 var jsonData;
 var index = 0;
 var pop = 0;
 var length;
-recognition.lang = "en-GB";
+var recognition = new webkitSpeechRecognition();
+recognition.lang = "en-US";
 recognition.onresult = function(event) {
   console.log(event)
+  //get what was said in the mic
   song = event.results[0][0].transcript;
   console.log(song)
+  // query spotify for it
   $.get( "http://ws.spotify.com/search/1/track.json?q="+ song, function( data ) {
     console.log(data)
+    // check if the query returned any song
     if(data.info.num_results === 0){
-      alert("Search failed, Sorry")
+      alert("Search failed. Sorry, but you can try again")
     } else {
-      jsonData = data.tracks;
-      console.log(jsonData);
-      length = jsonData.length;
+      songs = data.tracks;
+      console.log(songs);
+      //just caching array length
+      length = songs.length;
+      //loop over the array to get most popular one
       for(var i = 0; i < length; i++){
-        if(jsonData[i].popularity > pop){
-          pop = jsonData[i].popularity;
+        if(songs[i].popularity > pop){
+          pop = songs[i].popularity;
           index = i;
         }
       }
-      var url = "https://open.spotify.com/track/" + jsonData[index].href.split(":")[2]
+      // build the url to redirect
+      var url = "https://open.spotify.com/track/" + songs[index].href.split(":")[2]
       console.log(url);
-      window.location=url;     
+      // redirect user to spotify
+      window.location = url;     
     }
   });
 }
@@ -38,6 +45,7 @@ recognition.onresult = function(event) {
   option to get more results
   Make a css file, this looks bad
   make it deal with errors more gracefully
+  sort the array by popularity, instead of just selecting most popular song
   find a lyrics api
     let the user choose wether hes saying song title/band title or lyrics
     in case its a lyric, query the lyric api
