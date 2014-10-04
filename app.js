5c581ed283c978f705f8a1f069522e75e783347e
iagowp <iago.wp@hotmail.com>
2014-10-03 21:49:58 -0700

    better documented whats next steps, changed text on index.html

--- app.js
+++ app.js
@@ -11,18 +11,22 @@ recognition.onresult = function(event) {
   console.log(song)
   $.get( "http://ws.spotify.com/search/1/track.json?q="+ song, function( data ) {
     console.log(data)
-    jsonData = data.tracks;
-    console.log(jsonData);
-    length = jsonData.length;
-    for(var i = 0; i < length; i++){
-      if(jsonData[i].popularity > pop){
-        pop = jsonData[i].popularity;
-        index = i;
+    if(data.info.num_results === 0){
+      alert("Search failed, Sorry")
+    } else {
+      jsonData = data.tracks;
+      console.log(jsonData);
+      length = jsonData.length;
+      for(var i = 0; i < length; i++){
+        if(jsonData[i].popularity > pop){
+          pop = jsonData[i].popularity;
+          index = i;
+        }
       }
+      var url = "https://open.spotify.com/track/" + jsonData[index].href.split(":")[2]
+      console.log(url);
+      window.location=url;     
     }
-    var url = "https://open.spotify.com/track/" + jsonData[index].href.split(":")[2]
-    console.log(url);
-    window.location=url;     
   });
 }
 
@@ -31,5 +35,9 @@ recognition.start();
 /* todo: 
   make pure javascript ajax call
   give options to choose language
-  give options to choose in terms of popularity
+  give options to choose in terms of popularity -> right now is most popular, make it able to choose least popular, or a determined number
+  option to get more results
+  Make a css file, this looks bad
+  create a button to start the app
+  make it deal with errors more gracefully
 */
