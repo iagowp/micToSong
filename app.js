78ef72042a187318aaa3de04a9398440b817fca3
iagowp <iago.wp@hotmail.com>
2014-10-16 20:21:48 -0700

    cleaned up the code a lot, refactored it a bit, minor changes to style

--- app.js
+++ app.js
@@ -1,68 +1,67 @@
 // https have a problem with calling spotify api with a http protocol, so I force http
-if(window.location.href === "https://iagowp.github.io/micToSong/") window.location = "http://iagowp.github.io/micToSong/";
-
+if(window.location.href === "https://iagowp.github.io/micToSong/"){
+  window.location = "http://iagowp.github.io/micToSong/";
+}
+// variable used to control wether or not the ajax function is running when the recognition triggers onend
 var started = false;
-
 var recognition = new webkitSpeechRecognition();
 recognition.onresult = function(event) {
   started = true;
-  console.log(event)
   //get what was said in the mic
   var song = event.results[0][0].transcript;
-  console.log(song)
-  // query spotify for it
+  console.log(song);
+  // making a ajax request, and sending function to be executed with result
+  query(song, function(data){
+    resultsNum = data.info.num_results; // this property can be used to return more results
+    // check if the query returned any song
+    if(resultsNum !== 0){
+      var songs = data.tracks;
+      var index = document.getElementById('popularity').value;
+      // select is on random
+      if(index === 'r'){
+        index =  Math.floor(Math.random() * 99);
+      }
+      // put it on the last song, in case the result is smaller than the option chosen
+      if(index > resultsNum){
+        index = resultsNum-1;
+      }
+      // build the url to redirect
+      var url = "https://open.spotify.com/track/" + songs[index].href.split(":")[2];
+      // redirect user to spotify
+      window.location = url;   
+    } else {
+      alert("Search failed. Sorry, try again please.");
+    }
+  });
 
-  // pure js ajax call
+};
+// make a query to spotify, and apply callback on result
+var query = function(song, callback){
   var request = new XMLHttpRequest();
 
   request.onreadystatechange = function(){
     // state 4 is when it gets a result
     if(request.readyState === 4){
+      // parse JSON result from ajax
       var data = JSON.parse(request.responseText);
 
-      console.log(data)
-
-      resultsNum = data.info.num_results; // this property can be used to return more results
-      // check if the query returned any song
-      if(resultsNum !== 0){
-        var songs = data.tracks;
-        console.log(songs);
-
-        var index = document.getElementById('popularity').value;
-
-        if(index === 'r') index =  Math.floor(Math.random() * 99);
-
-        if(index > resultsNum) index = resultsNum-1;
-
-        console.log(index);
-        
-        // build the url to redirect
-        var url = "https://open.spotify.com/track/" + songs[index].href.split(":")[2]
-        console.log(url);
-        // redirect user to spotify
-        window.location = url;   
-      } else {
-        alert("Search failed. Sorry, try again please.")
-      }
+      callback(data);
     }
   };
 
   request.open("GET", "http://ws.spotify.com/search/1/track.json?q="+ song, true);
-
   request.send();
 };
-
 //when it ends, and theres no result, warn person
 recognition.onend = function(){
   if(started === false){
-    alert("Sorry, I couldn't understand what you said, could you try again?")
+    alert("Sorry, I couldn't understand what you said, could you try again?");
   } else {
     started = false;
   }
-}
+};
 
 var appStart = function(){
-  // window.focus();
   recognition.lang =  document.getElementById('language').value;
   recognition.start();
 };
